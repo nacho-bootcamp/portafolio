7e180dfb9cbfe8df135c532d0b99a6e46570ce02
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-07-18 20:46:50 -0300

    nav complete

--- src/Components/Head/Home.jsx
+++ src/Components/Head/Home.jsx
@@ -3,13 +3,13 @@ import logo from "../../assets/img/logotipo1.png";
 
 function Home() {
   return (
-    <header className="flex w-full items-center p-4">
-      <div className="w-1/6">
-        <img className="w-110 h-120" src={logo} alt="logotipo" />
+    <header className="flex w-full items-center p-1 h-[10vh]">
+      <div className="w-1/6 ">
+        <img className="w-24 h-24" src={logo} alt="logotipo" />
       </div>
-      <nav className="flex-1 flex items-center justify-center gap-10">
-        <a href="" className="text-one">
-          Inico
+      <nav className="w-2/3 flex items-center justify-center gap-12 text-base ">
+        <a href="" className="text-one font-sans">
+          Inicio
         </a>
         <a href="" className="text-one">
           Habilidades
@@ -21,11 +21,13 @@ function Home() {
           Contacto
         </a>
       </nav>
-      <ul className="flex item-center gap-4 p-4 bg-green-600 ">
-        <li>GitHub</li>
-        <li>Gmail</li>
-        <li>Linkedin</li>
-      </ul>
+      <div className="flex justify-">
+        <ul className="flex item-center gap-5">
+          <li>GitHub</li>
+          <li>Gmail</li>
+          <li>Linkedin</li>
+        </ul>
+      </div>
     </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,6 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    spacing: {
-      px: "1px",
-      110: "110px",
-      120: "120px",
-    },
     extend: {
       colors: {
         one: "#403F4C",
